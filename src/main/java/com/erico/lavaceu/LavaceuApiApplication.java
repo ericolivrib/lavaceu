f61af74ad5ddbf393a8eee9e1c519460c3005d3c
Erico <oliv.ericorib@gmail.com>
2024-01-03 14:31:45 -0300

    feat: iniciar o projeto

---
+++ src/main/java/com/erico/lavaceu/LavaceuApiApplication.java
@@ -0,0 +1,13 @@
+package com.erico.lavaceu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaceuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavaceuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erico/lavaceu/LavaceuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erico.lavaceu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaceuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
